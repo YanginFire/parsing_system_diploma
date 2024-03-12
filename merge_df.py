4c1d632fa084d118a827a87cf943e70ccfd6bafa
YanginFire <irika.kudinova@yandex.ru>
2024-03-12 20:55:08 +0300

    streamlit web site deployment + data science

--- merge_df.py
+++ merge_df.py
@@ -1,24 +1,23 @@
 import pandas as pd
 import os
-from datetime import date
 
-# Получаем список файлов в папке
-folder_path = 'C:/Users/Yangin/Desktop/VKR/parsing_system_diploma/hh'
+# Получаем список файлов Excel в папке
+folder_path = 'C:/Users/User/Desktop/diploma'
 files = os.listdir(folder_path)
 
-# Фильтруем файлы и создаем список путей к файлам xlsx, начинающимся с "vacancies"
-xlsx_files = [os.path.join(folder_path, file) for file in files if file.endswith('.xlsx') and file.startswith('vacancies')]
+# Фильтруем файлы и создаем список путей к файлам Excel
+excel_files = [os.path.join(folder_path, file) for file in files if file.endswith('.xlsx')]
 
 # Создаем пустой датафрейм, в который будем добавлять данные из файлов
 combined_df = pd.DataFrame()
 
-# Читаем и объединяем данные из файлов xlsx в единый датафрейм
-for file_path in xlsx_files:
+# Читаем и объединяем данные из файлов Excel в единый датафрейм
+for file_path in excel_files:
     df = pd.read_excel(file_path)
-    combined_df = combined_df.append(df, ignore_index=True)
+    combined_df = combined_df._append(df, ignore_index=True)
 
-# Создаем новый CSV-файл и сохраняем в него данные с кодировкой UTF-8
-output_file = 'df_merged2023-11-13.xlsx'
-combined_df.to_excel(output_file, index=False, encoding='utf-8')
+# Создаем новый Excel-файл и сохраняем в него данные
+output_file = 'C:/Users/User/Desktop/diploma/df_merged_data_october_march.xlsx'
+combined_df.to_excel(output_file, index=False)
 
 print("Данные сохранены в файл:", output_file)
---
+++ streamlit_web_app/web_app_analytic_system.py
@@ -0,0 +1,51 @@
+import plotly.express as px
+import pandas as pd
+import streamlit as st
+
+# инициализация конфига для шапки веб-сайта
+
+st.set_page_config(page_title="Dashboard Vacancy Analysis", page_icon=":bar_chart:", layout='wide')
+
+# загрузка датафрейма на сайт
+
+df = pd.read_excel(
+    io='C:/Users/User/Desktop/diploma/clear_dataframe.xlsx',
+    engine='openpyxl',
+    sheet_name="Sheet1",
+    usecols="B:I",
+)
+
+# инициализацяи бокового меню для фильтров
+st.sidebar.header("Выберите нужные фильтры для Вас:")
+
+# фильтр графика работы
+time_schedule_filter = st.sidebar.multiselect(
+    "Выберите фильтр для поля 'График работы': ",
+    options=df["График работы"].unique(),
+    default=df["График работы"].unique()
+)
+# фильтр опыта работы
+working_experience_filter = st.sidebar.multiselect(
+    "Выберите фильтр для поля 'Требуемый опыт': ",
+    options=df["Требуемый опыт"].unique(),
+    default=df["Требуемый опыт"].unique()
+)
+
+# Запрос к датафрейму для осуществелния обновления таблицы
+# по тем фильтрам кототрые задает пользователь системы
+
+selection_query = df.query("`График работы` == @time_schedule_filter & `Требуемый опыт` == @working_experience_filter")
+
+st.header('Dashboard Vacancy Analysis :bar_chart:', divider='rainbow')
+
+st.caption('Данное веб-приложение предназначено для просмотра актуальности професиии и необходимых статистических '
+           'данных отрасли IT специальностей. Здесь вы можете просмотреть такие данные как Зарплата, '
+           'Компании-работадатели, средняя зарплата по определенной спцеальности и многое другое. Данное приложение '
+           'подойдет не только для соискателей, но и для работадателей')
+st.divider()
+
+
+
+# подключение базы данных
+st.dataframe(selection_query)
+
